76a325c36d2a44df0e03a2abeb4991dffa6dca75
Mustafa Redifoglu <mustafaredifoglu@gmail.com>
2024-11-20 11:19:29 +0300

    all tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,35 @@
 package org.example;
 
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("I did, did I?"));
+        System.out.println(checkForPalindrome("Racecar "));
+        System.out.println(checkForPalindrome("hello"));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+
+    }
+
+    public static boolean checkForPalindrome(String string){
+        String text = string.replaceAll("[?.,!_\\- ]","").toLowerCase(Locale.ROOT);
+        StringBuilder mahmut = new StringBuilder(text).reverse();
+        return text.equals(mahmut.toString());
+    }
+
+    public static String convertDecimalToBinary(int number){
+        String binary = "";
+        while(number > 0){
+            binary += String.valueOf(number % 2);
+            number = number / 2;
+        }
+        StringBuilder newBinary = new StringBuilder(binary);
+        return newBinary.reverse().toString();
     }
 
 }
